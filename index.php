b0c348f1d3181f61a8ac616ae8d715fa2fc5a990
#Баярхүү <beat5friends@gmail.com>
2020-04-22 19:01:20 +0800

    ui added

--- index.php
+++ index.php
@@ -6,21 +6,48 @@
 <body>
 <script type="text/javascript" src = "js/app.js"></script>
 
-
 <div class="calculator">
+    <div class="header-section">
+        CASIO
+    </div>
         <div class="section2">
             <div class="screen">
             123456789
             </div>
         </div>
-        <div class="calc-row">
-            <div class="button-clear">C</div>
-            <div class="button">0</div>
-            <div class="button-dot">.</div>
-            <div class="button">=</div>
-            <div class="button-equal">=</div>
-            <div class="button-plus">+</div>
-        </div>
+      <div class="buttons">
+        <table>   
+            <tr>
+                <td><div class="btn-clear">C</div></td>
+                <td><div class="btn-plus-minus">+/-</div></td>
+                <td><div class="btn-100">%</div></td>
+                <td><div class="btn-nine">|</div></td>
+
+            </tr>
+            <tr>
+                <td><div class="btn-seven">7</div></td>
+                <td><div class="btn-eigth">8</div></td>
+                <td><div class="btn-nine">9</div></td>
+                <td><div class="btn-x">x</div></td>
+            </tr>
+            <tr>
+                <td><div class="btn-four">4</div></td>
+                <td><div class="btn-five">5</div></td>
+                <td><div class="btn-six">6</div></td>
+                <td><div class="btn-minus">-</div></td>
+            </tr>
+            <tr>
+                <td><div>1</div></td>
+                <td><div>2</div></td>
+                <td><div>3</div></td>
+                <td><div>+</div></td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+        </table>
+      </div>
+
 </div>
 </body>
 </html>
